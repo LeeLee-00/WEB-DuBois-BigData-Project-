0aeb57da89128fa32911c8f37b144cf4521b53a7
Lee Joey Noel <Leenoel@live.com>
2022-05-23 18:22:14 -0400

    Fix code for plate 3 visualization for 2016 and 2020

--- R/2020 State Population Bar Graph - Plate 3.R
+++ R/2020 State Population Bar Graph - Plate 3.R
@@ -1,5 +1,5 @@
 library(dplyr)
-
+library(ggplot2)
 
 # Previously imported and cleaned data using Cleaning Census Voting Data Script
 # Filtered for "Black alone"
@@ -15,32 +15,32 @@ BlackPopulation <- BlackPopulation[-1, ]
 
 ### Adapted from Ella Hollowood (https://rpubs.com/ejhollowood/du-bois) ###
 
-top_n(BlackPopulation, n = 15, Population) %>%
-  ggplot(., aes(x = reorder(State, Population), y = Population)) +
-  geom_bar(
-    stat = "identity", 
-    width = 0.5, 
-    fill = "red3") +
-  coord_flip() +
-  geom_text(data = subset(BlackPopulation, Population >= 2700), 
-            aes(y = Population, ymax = Population, label = Population),
-            hjust = 13,
-            family = c("Rajdhani"), size = 5) +
-  geom_text(data = subset(BlackPopulation, Population == 994), 
-            aes(y = Population, ymax = Population, label = Population),
-            hjust = 6,
-            family = c("Rajdhani"), size = 5) +
-  theme(text = element_text(family = "Rajdhani", colour = "black"),
-        panel.background = element_blank(),
-        axis.title = element_blank(),
-        axis.text.x = element_blank(),
-        axis.text.y = element_text(size = 16),
-        axis.ticks = element_blank(),
-        panel.grid = element_blank(),
-        plot.title = element_text(size = 24, hjust = 0.5, 
-                                  face = "bold", margin = margin(0,0,30,0))) +
-  labs(title = "2020 THE STATES OF THE UNITED STATES ACCORDING\nTO THEIR BLACK POPULATION.")
+BlackPopulation <- top_n(BlackPopulation, n = 15, Population) %>%
+    ggplot(., aes(x = reorder(State, Population), y = Population)) +
+    geom_bar(
+      stat = "identity", 
+      width = 0.5, 
+      fill = "red3") +
+    coord_flip() +
+    geom_text(data = subset(BlackPopulation, Population >= 2700), 
+              aes(y = Population, ymax = Population, label = Population),
+              hjust = 13,
+              family = c("Rajdhani"), size = 5) +
+    geom_text(data = subset(BlackPopulation, Population == 994), 
+              aes(y = Population, ymax = Population, label = Population),
+              hjust = 6,
+              family = c("Rajdhani"), size = 5) +
+    theme(text = element_text(family = "Rajdhani", colour = "black"),
+          panel.background = element_blank(),
+          axis.title = element_blank(),
+          axis.text.x = element_blank(),
+          axis.text.y = element_text(size = 16),
+          axis.ticks = element_blank(),
+          panel.grid = element_blank(),
+          plot.title = element_text(size = 24, hjust = 0.5, 
+                                    face = "bold", margin = margin(0,0,30,0))) +
+    labs(title = "2020 THE STATES OF THE UNITED STATES ACCORDING\nTO THEIR BLACK POPULATION.")
 
+# Call Plot
+BlackPopulation
 ### Code above adapted from Ella Hollowood (https://rpubs.com/ejhollowood/du-bois) ###
-
-
